2d8f86ebea1a4daeec03ac0c05d12c766f6672d9
MJ Pickin <mjp513@research0.york.ac.uk>
2017-12-01 18:40:28 +0000

    Added functionality to change permissions:  group byte is set to world byte

--- perm_mapper.rb
+++ perm_mapper.rb
@@ -5,6 +5,16 @@ def currentPermissions(file)
 	file.stat.mode.to_s(8).chars.last(3)
 end
 
+def setITSPermissions(file)
+	permissions = currentPermissions(file)
+	# Need to set group permissions to the same as world permissions
+	migratedPermissions = "0" + permissions[0] + permissions[2] + permissions[2]
+        # puts migratedPermissions.to_i(8)
+	file.chmod(migratedPermissions.to_i(8))
+        # puts file.stat.mode.to_s(8)
+end
+
 filename=ARGV[0]
 file=File.open(filename)
-puts currentPermissions(file)
+setITSPermissions(file)
+puts file.stat.mode.to_s(8)
